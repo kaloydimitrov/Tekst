c4c23193162605298cc45db7f6fd2e0b389a9ad4
kaloydimitrov <kalotablet2006@gmail.com>
2024-05-12 02:15:38 +0300

    removing vue demo

--- routing/views.py
+++ routing/views.py
@@ -1,20 +1,5 @@
 from django.shortcuts import render
-import random
-import json
 
 
 def home(request):
-    names = ("bob", "dan", "jack", "lizzy", "susan")
-
-    items = []
-    for i in range(100):
-        items.append({
-            "name": random.choice(names),
-            "age": random.randint(20, 80),
-            "url": "https://example.com",
-        })
-
-    context = {}
-    context["items_json"] = json.dumps(items)
-
-    return render(request, 'index.html', context)
+    return render(request, 'index.html')
