22dc05949c7d9d8f665e0cd2e614a8c25e555e76
kaloydimitrov <kalotablet2006@gmail.com>
2024-06-04 17:11:15 +0300

    space and tags - full functionality

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from django.urls import include
-from .views import SpaceDetailView, SpaceListView
+from .views import SpaceDetailView, SpaceListView, TagListView
 
 urlpatterns = [
     path('space/', include([
         path('all/', SpaceListView.as_view(), name='get_all_spaces'),
         path('<int:pk>/', SpaceDetailView.as_view(), name='get_space_details'),
     ])),
+    path('tags/', include([
+        path('all/', TagListView.as_view(), name='get_all_tags')
+    ]))
 ]
--- api/views.py
+++ api/views.py
@@ -1,8 +1,8 @@
 from rest_framework import generics
 from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.permissions import IsAuthenticated
-from spaces.models import Space
-from .serializers import SpaceSerializer
+from spaces.models import Space, Tag
+from .serializers import SpaceSerializer, TagSerializer
 
 
 # --------------------------------------
@@ -20,3 +20,14 @@ class SpaceDetailView(generics.RetrieveAPIView):
     permission_classes = [IsAuthenticated]
     queryset = Space.objects.all()
     serializer_class = SpaceSerializer
+
+
+# --------------------------------------
+# TAGS
+# --------------------------------------
+class TagListView(generics.ListAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+    filter_backends = (SearchFilter, OrderingFilter)
+    search_fields = ('name', )
