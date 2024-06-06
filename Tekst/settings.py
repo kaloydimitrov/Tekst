c3d6f2616822ce1e2a74cd4942a4aa26234a4fdf
kaloydimitrov <kalotablet2006@gmail.com>
2024-06-06 17:59:05 +0300

    pagination

--- Tekst/settings.py
+++ Tekst/settings.py
@@ -78,6 +78,15 @@ TEMPLATES = [
 WSGI_APPLICATION = 'Tekst.wsgi.application'
 
 
+# Django Rest Framework
+# https://www.django-rest-framework.org/
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
+
+
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
