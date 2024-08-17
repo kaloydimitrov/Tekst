cc276049e2e2efd7f956ef2e82d4323b9e127a1d
kaloydimitrov <kalotablet2006@gmail.com>
2024-08-17 22:48:25 +0300

    custom tags

---
+++ routing/templatetags/custom_tags.py
@@ -0,0 +1,26 @@
+from django import template
+from spaces.models import Space, UserSpaceFollow
+from authentication.models import UserFollows
+from django.db.models import Count
+
+register = template.Library()
+
+
+@register.simple_tag
+def get_verified_spaces():
+    return Space.objects.filter(verified=True).order_by('-created_at')[:5]
+
+
+@register.simple_tag
+def get_top_spaces():
+    return Space.objects.annotate(num_followers=Count('followers')).order_by('-num_followers')
+
+
+@register.simple_tag
+def get_followed_spaces(user):
+    return UserSpaceFollow.objects.filter(user=user).order_by('-created_at')
+
+
+@register.simple_tag
+def get_followed_users(user):
+    return UserFollows.objects.filter(follower=user)
--- spaces/admin.py
+++ spaces/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Space, Tag
+from .models import Space, Tag, UserSpaceFollow
 
 admin.site.register(Space)
 admin.site.register(Tag)
+admin.site.register(UserSpaceFollow)
